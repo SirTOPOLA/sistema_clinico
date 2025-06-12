13060def94e89d1c404a64d4430f50b71ce88653
SIR-TOPOLÁ <sirtopola@gmail.com>
2025-06-12 11:59:34 +0100

    clini waadasjass

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,6 @@
-
 </div>
 
- 
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
@@ -20,37 +19,37 @@
         })
     })()
 
-   
+
     function cerrarSession() {
         document.getElementById('cerrarSession').addEventListener('click', () => {
             if (confirm('¿Estás seguro de que deseas cerrar la sesión?')) {
-                window.location.href = 'logout.php';  
+                window.location.href = 'logout.php';
                 location.reload()
             }
         });
     }
 
-    function sidebarToggle () { 
-document.getElementById("toggleSidebar").addEventListener("click", function () {
-    if (window.innerWidth <= 767.98) {
-        document.getElementById("sidebar").classList.toggle("show");
-        document.getElementById("sidebar").classList.remove("collapsed");
-        document.getElementById("content").classList.remove("collapsed");
-        document.getElementById("navbar").classList.remove("collapsed");
- 
-    } else {
-        document.getElementById("sidebar").classList.toggle("collapsed");
-        document.getElementById("content").classList.toggle("collapsed");
-        document.getElementById("navbar").classList.toggle("collapsed");
+    function sidebarToggle() {
+        document.getElementById("toggleSidebar").addEventListener("click", function () {
+            if (window.innerWidth <= 767.98) {
+                document.getElementById("sidebar").classList.toggle("show");
+                document.getElementById("sidebar").classList.remove("collapsed");
+                document.getElementById("content").classList.remove("collapsed");
+                document.getElementById("navbar").classList.remove("collapsed");
+
+            } else {
+                document.getElementById("sidebar").classList.toggle("collapsed");
+                document.getElementById("content").classList.toggle("collapsed");
+                document.getElementById("navbar").classList.toggle("collapsed");
+
+            }
+        });
 
     }
-});
 
-}
-
-document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
         cerrarSession();
-        sidebarToggle ()
+        sidebarToggle()
     });
 </script>
 
